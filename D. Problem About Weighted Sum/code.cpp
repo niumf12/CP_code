734c48afe0f833701f01c7ad4d2a636c7ffa87a5
niumf12 <Tranvanducanh08112008@gmail.com>
2026-01-14 19:00:47 +0700

    commit

--- D. Problem About Weighted Sum/code.cpp
+++ D. Problem About Weighted Sum/code.cpp
@@ -45,7 +45,8 @@ struct SMT {
     void upd_fix(int id, int l, int r, int p, int val) { 
         if (p < l || r < p) return; 
         if (l == r) { 
-            st[id].fixed = val; 
+            st[id].fixed += val; 
+            st[id].sum += val * l; 
             return; 
         }
         int mid = (l + r) >> 1; 
@@ -64,6 +65,10 @@ struct SMT {
         node &mm = st[id];
         ll.sum += sumlr(l, mid) * mm.d_val; 
         rr.sum += sumlr(mid + 1, r) * mm.d_val; 
+        ll.fixed += (mid - l + 1) * mm.d_val; 
+        rr.fixed += (r - mid) * mm.d_val; 
+        ll.d_val += mm.d_val; 
+        rr.d_val += mm.d_val; 
         mm.d_val = 0; 
     }
 
@@ -71,6 +76,7 @@ struct SMT {
         if (v < l || r < u) return; 
         if (u <= l && r <= v) { 
             st[id].sum += sumlr(l, r) * val; 
+            st[id].fixed += val * (r - l + 1); 
             st[id].d_val += val; 
             return;
         }
@@ -83,15 +89,42 @@ struct SMT {
     void upd(int u, int v, int val) { 
         upd(1, 1, n, u, v, val); 
     }
-};
+
+    node get(int id, int l, int r, int u, int v) { 
+        if (v < l || r < u) return node(); 
+        if (u <= l && r <= v) return st[id]; 
+        int mid = (l + r) >> 1; 
+        pushDown(id, l, r, mid);
+        return combine(get(id << 1, l, mid, u, v), get(id << 1 | 1, mid + 1, r, u, v)); 
+    }
+    int get(int u, int v) { 
+        node tmp = get(1, 1, n, u, v); 
+        return -(u - 1) * tmp.fixed + tmp.sum; 
+    }
+} tree; 
 
 void solve() { 
     cin >> n >> m; 
     FOR(i, 1, n) cin >> a[i]; 
-
+    tree = SMT(n); 
+    FOR(i, 1, n) { 
+        tree.upd_fix(i, a[i]); 
+    }
+    FOR(i, 1, m) { 
+        int type, l, r, d; 
+        cin >> type >> l >> r; 
+        if (type == 1) { 
+            cin >> d; 
+            tree.upd(l, r, d); 
+        } else { 
+            cout << tree.get(l, r) << '\n'; 
+        }
+    }
 }
 
 int32_t main() { 
+    freopen("test.inp","r",stdin); 
+    freopen("test.out","w",stdout); 
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr); 
     solve(); 
